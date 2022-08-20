04dad78c6d16d8ef54a35dd7a925f2cd9b3bea8f
haymeemotoh22@gmail.com <haymeemotoh22@gmail.com>
2022-08-20 19:28:30 +0800

    Project Organization In-Progress

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -1,5 +1,8 @@
 extends Node
 
+# Singleton
+# This Script is loaded at the start of the game application
+
 var levelScene setget _setWhereLevelScene, _getWhereLevelScene
 
 func _setWhereLevelScene(value):
--- Scripts/Item.gd
+++
@@ -1,7 +0,0 @@
-extends Resource
-class_name ItemResource
-
-export var name : String
-export var stackable : bool = true
-export var maxStackSize : int = 10
-export var texture : Texture
